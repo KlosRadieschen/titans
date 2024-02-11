ed4e8f1c4a67b5bb25437a121f622999a3c4aec9
Nicolas <nicolas@wagener.family>
2024-02-11 15:52:37 +0000

    added bad table

--- go.mod
+++ go.mod
@@ -4,11 +4,13 @@ go 1.21.1
 
 require (
 	github.com/bwmarrin/discordgo v0.27.1
+	github.com/olekukonko/tablewriter v0.0.5
 	github.com/sashabaranov/go-openai v1.17.11
 )
 
 require (
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
 	golang.org/x/crypto v0.9.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
 )
--- scorch.go
+++ scorch.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"os"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/olekukonko/tablewriter"
 )
 
 func reactReceived(s *discordgo.Session, r *discordgo.MessageReactionAdd) {
@@ -18,12 +18,34 @@ func reactReceived(s *discordgo.Session, r *discordgo.MessageReactionAdd) {
 }
 
 func updateList(s *discordgo.Session) {
-	var response string
 	var lowerCaseNames = []string{"pc", "xbox", "playstation", "ps4", "ps5"}
 	var displayNames = []string{"PC", "Xbox", "Playstations", "PS4", "PS4"}
+	var data [][]string
 
 	for i := range lowerCaseNames {
-		response = appendEntryFromFile(response, lowerCaseNames[i], displayNames[i])
+		file, _ := os.OpenFile("/home/Nicolas/go-workspace/src/titans/members.csv", os.O_APPEND|os.O_RDWR|os.O_SYNC, os.ModeAppend)
+		defer file.Close()
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			parts := strings.Split(scanner.Text(), ",")
+			if len(parts) == 3 && strings.ToLower(parts[2]) == lowerCaseNames[i] {
+				member, err := s.GuildMember("1195135473006420048", parts[0])
+				if err != nil {
+					panic(parts[0] + ": " + err.Error())
+				}
+				rank, _ := s.State.Role("1195135473006420048", member.Roles[0])
+				var battalionName string
+				if len(member.Roles) < 2 {
+					battalionName = ""
+				} else {
+					battalion, _ := s.State.Role("1195135473006420048", member.Roles[1])
+					battalionName = battalion.Name
+				}
+				var row = []string{displayNames[i], member.Mention(), parts[1], rank.Name, battalionName}
+				data = append(data, row)
+				data = append(data, []string{""})
+			}
+		}
 	}
 
 	file, _ := os.OpenFile("/home/Nicolas/go-workspace/src/titans/members.csv", os.O_APPEND|os.O_RDWR|os.O_SYNC, os.ModeAppend)
@@ -32,32 +54,39 @@ func updateList(s *discordgo.Session) {
 	for scanner.Scan() {
 		parts := strings.Split(scanner.Text(), ",")
 		if len(parts) == 3 && strings.ToLower(parts[2]) != "playstation" && strings.ToLower(parts[2]) != "pc" && strings.ToLower(parts[2]) != "xbox" && strings.ToLower(parts[2]) != "ps4" && strings.ToLower(parts[2]) != "ps5" {
-			response += fmt.Sprintf("%s: %s\n", "**"+parts[2]+"**", parts[1])
+			user, _ := s.GuildMember("1195135473006420048", parts[0])
+			rank, _ := s.State.Role("1195135473006420048", user.Roles[0])
+			var battalionName string
+			if len(user.Roles) < 2 {
+				battalionName = ""
+			} else {
+				battalion, _ := s.State.Role("1195135473006420048", user.Roles[1])
+				battalionName = battalion.Name
+			}
+
+			var row = []string{parts[2], user.Mention(), parts[1], rank.Name, battalionName}
+			data = append(data, row)
 		}
 	}
 
+	var builder strings.Builder
+	writer := &builder
+	table := tablewriter.NewWriter(writer)
+	//table.SetHeader([]string{"Platform", "User", "in-game name", "Rank", "Battalion"})
+	table.SetBorders(tablewriter.Border{Left: false, Top: false, Right: false, Bottom: false})
+	table.SetCenterSeparator("|")
+	table.AppendBulk(data)
+	table.Render()
+
 	embed := &discordgo.MessageEmbed{
 		Title:       "Registered members:",
-		Description: response,
+		Description: builder.String(),
 		Color:       0xF73718,
 	}
 	_, err := s.ChannelMessageEditEmbed("1196072273686315008", "1196079691577163798", embed)
 	if err != nil {
 		panic(err.Error())
 	}
-}
-
-func appendEntryFromFile(response string, lowerCaseName string, displayName string) string {
-	file, _ := os.OpenFile("/home/Nicolas/go-workspace/src/titans/members.csv", os.O_APPEND|os.O_RDWR|os.O_SYNC, os.ModeAppend)
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		parts := strings.Split(scanner.Text(), ",")
-		if len(parts) == 3 && strings.ToLower(parts[2]) == lowerCaseName {
-			response += fmt.Sprintf("%s: %s\n", "**"+displayName+"**", parts[1])
-		}
-	}
-	return response
 }
 
 func guildMemberAdd(s *discordgo.Session, m *discordgo.GuildMemberAdd) {
